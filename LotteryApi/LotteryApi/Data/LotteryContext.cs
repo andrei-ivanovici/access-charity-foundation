58156ffcab73e4e5c60aaf8c56867cd4638f0b5e
Andrei Ivanovici <andrei.ivanovici@gmail.com>
2020-01-30 23:29:24 +0200

    save delete payment methods

--- LotteryApi/LotteryApi/Data/LotteryContext.cs
+++ LotteryApi/LotteryApi/Data/LotteryContext.cs
@@ -3,15 +3,24 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LotteryApi.Data
 {
-    public class LotteryContext: DbContext
+    public class LotteryContext : DbContext
     {
         public DbSet<UserEntity> Users { get; set; }
+        public DbSet<PaymentEntity> Payments { get; set; }
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(
                 @"Server=tcp:testserver01.database.windows.net,1433;Initial Catalog=lottery;Persist Security Info=False;User ID=kiko;Password=9x5U3nbyvXMi;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PaymentEntity>()
+                .HasOne<UserEntity>(s => s.User)
+                .WithMany(g => g.Payments)
+                .HasForeignKey(s => s.UserId);
         }
     }
 }
