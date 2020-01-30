454b268cece0aa1deb211c8ff674398f203646c0
Andrei Ivanovici <aivanovici@isoftwaresystems.com>
2020-01-30 12:45:56 +0200

    mock login flow

--- lottery/src/Root/Root.tsx
+++ lottery/src/Root/Root.tsx
@@ -1,16 +1,23 @@
-import React from "react";
-import {BrowserRouter, Route, Redirect, Switch, Router} from "react-router-dom";
+import React, {useEffect, useMemo, useState} from "react";
+import {Redirect, Route, Router, Switch} from "react-router-dom";
 import {GuardedRoute} from "../components/GuardedRoute";
 import {App} from "../shell/App";
 import {LogIn} from "../authorization/login/LogIn";
 import {appHistory} from "../services/navigation.service";
 import {Register} from "../authorization/register/Register";
+import {loginService, User} from "../services/login.service";
 
 
 function useAuthorization() {
 
+    const [activeUser, setActiveUse] = useState<User>();
+
+    useEffect(() => {
+        loginService.observeUserChange(setActiveUse)
+    }, []);
+
     return {
-        isAuthorized: false
+        isAuthorized: !!activeUser,
     }
 }
 
@@ -33,6 +40,7 @@ export function Root() {
                               onSuccess={() => <App/>}
                               onFail={() => <Redirect to={"/login"}/>}
                 />
+
             </Route>
         </Switch>
     </Router>
--- lottery/src/services/login.service.ts
+++ lottery/src/services/login.service.ts
@@ -4,11 +4,22 @@ export interface User {
     avatar?: string;
 }
 
+type  Listener = (u: User) => void
+
 export class LoginService {
     public user?: User;
+    private _listeners: Listener[] = [];
 
     public async login(username: string, passwor: string): Promise<void> {
+        this.user = {
+            email: username,
 
+        } as any;
+        this._listeners.forEach(l => this.user && l(this.user))
+    }
+
+    public observeUserChange(observer: Listener) {
+        this._listeners.push(observer);
     }
 }
 
