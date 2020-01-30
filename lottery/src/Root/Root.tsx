7f5e5f4ba5ac07c922381a51c47e14bdd71f0052
Anita Fallon <afallon@isoftwaresystems.com>
2020-01-30 13:29:29 +0200

    Add buy ticket draft

--- lottery/src/Root/Root.tsx
+++ lottery/src/Root/Root.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import {BrowserRouter, Route, Redirect, Switch, Router} from "react-router-dom";
+import {BrowserRouter, Route, Redirect, Switch, Router, Link} from "react-router-dom";
 import {GuardedRoute} from "../components/GuardedRoute";
 import {App} from "../shell/App";
 import {LogIn} from "../authorization/login/LogIn";
 import {appHistory} from "../services/navigation.service";
 import {Register} from "../authorization/register/Register";
+import TicketWizard from "../ticket/TicketWizard/TicketWizard";
 
 
 function useAuthorization() {
@@ -27,6 +28,9 @@ export function Root() {
                               onSuccess={() => <LogIn/>}
                               onFail={() => <Redirect to={"/"}/>}
                 />
+            </Route>
+            <Route path={"/buy-ticket"}>
+                <TicketWizard/>
             </Route>
             <Route path={"/"}>
                 <GuardedRoute canNavigate={() => isAuthorized}
---
+++ lottery/src/ticket/BuyTicket/BuyTicket.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import  { TagWizard, TagWizardStep, TagFormGroup, TagEditField, TagField, TagList, TagButton } from "@tag/tag-components-react-v2";
+
+
+const BuyTicket: React.FC=()=>{
+return (
+    <TagButton text='Click me'/>
+)
+
+}
+
+export default BuyTicket;
