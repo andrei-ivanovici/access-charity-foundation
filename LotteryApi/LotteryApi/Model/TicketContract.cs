6c200445bf6754e148d6e838cf8960255cc0b372
mati.florin <mati.florin@gmail.com>
2020-01-31 17:51:47 +0200

    Adding TicketContact

---
+++ LotteryApi/LotteryApi/Model/TicketContract.cs
@@ -0,0 +1,15 @@
+﻿using LotteryApi.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LotteryApi.Model
+{
+    public class TicketContract
+    {
+        public int Id { get;  set; }
+        public int CharityId { get;  set; }
+        public int LotteryId { get; set; }
+    }
+}
