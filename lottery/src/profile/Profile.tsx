7f85b52b7eb4312903d7e688659ab334de7c8ed1
Cezar Fleseriu <cfleseriu@isoftwaresystems.com>
2020-01-31 10:04:06 +0200

    Remove LotteryEvent rendering

--- lottery/src/profile/Profile.tsx
+++ lottery/src/profile/Profile.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Lottery } from "../components/lottery/Lottery";
 
 export function Profile() {
-    return <><div>Profile</div><Lottery charities={[]} /></>
+    return <div>Profile</div>
 }
